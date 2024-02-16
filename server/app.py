489204882dc11580d6a67a44f3f1b2a6566283b0
Elikibs <elishakibet67@gmail.com>
2024-02-16 08:46:45 +0300

    update authentication

---
+++ server/app.py
@@ -0,0 +1,75 @@
+from flask import Flask, jsonify
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+from models import db, User, TokenBlocklist
+from blueprints.auth import auth_bp
+from blueprints.users import user_bp
+
+
+app = Flask(__name__)
+app.secret_key = '5f8e5e934051cdfb322c0619'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///church.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+# Initialize app with db, migrate
+migrate = Migrate(app, db)
+jwt = JWTManager(app)
+db.init_app(app)
+
+# register blueprints
+app.register_blueprint(auth_bp, url_prefix='/auth')
+app.register_blueprint(user_bp, url_prefix='/users')
+
+# load user
+@jwt.user_lookup_loader
+def user_looup_callback(jwt_header, jwt_data):
+    identity = jwt_data['sub']
+    return User.query.filter_by(email=identity).one_or_none()
+
+
+# jwt error handlers
+# expired token handler
+@jwt.expired_token_loader
+def expired_token_callback(jwt_header, jwt_data):
+    return jsonify(
+        {
+            "message": "Token has expired",
+            "error": "token_expired"
+        }
+    ), 401
+
+# invalid token loader
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+    return jsonify(
+        {
+            "message": "Signature verification failed",
+            "error": "invalid_token"
+        }
+    ), 401
+
+# unauthorized token loader (when token is not provided)
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+    return jsonify(
+        {
+            "message": "Request doesn't contain a valid token",
+            "error": "authorization_header"
+        }
+    ), 401
+
+
+# revoking tokens >> logout
+@jwt.token_in_blocklist_loader
+def token_in_blocklist_callback(jwt_header, jwt_data):
+    jti = jwt_data['jti']
+    
+    token = db.session.query(TokenBlocklist).filter(TokenBlocklist.jti == jti).scalar()
+
+    return token is not None
+
+
+
+if __name__== '__main__':
+    app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,7 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 
-db = SQLAlchemy
+db = SQLAlchemy()
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -9,98 +10,137 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
-    member_status = db.Column(db.Boolean, default=True)
-    admin_status = db.Column(db.Boolean, default=False)
-    super_admin_status = db.Column(db.Boolean, default=False)
+    password = db.Column(db.String(128))
 
-    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
-    personal_info_id = db.Column(db.Integer, db.ForeignKey('personal_info.id'))
-
-    personal_info = db.relationship('PersonalInfo', backref='user')
-    comments = db.relationship('Comment', backref='user')
-    donations = db.relationship('Donation', backref='user')
-    form_submissions = db.relationship('FormSubmission', backref='user')
-    roles = db.relationship('Role', backref='user')
-    events = db.relationship('Event', backref='user')
-
-class Role(db.Model):
-    __tablename__ = 'roles'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64))
-
-class PersonalInfo(db.Model):
-    __tablename__ = 'personalinfos'
-
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer)
-    full_name = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone_number = db.Column(db.String(120))
-    email = db.Column(db.String(120))
-
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    def __repr__(self):
+        return f"<User {self.email}>"
+    # set hash password
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
     
-class Comment(db.Model):
-    __tablename__ = 'comments'
+    # check if password is correct >> login
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+    
+    # check if user exists
+    @classmethod
+    def get_user_by_email(cls, email):
+        return cls.query.filter_by(email=email).first()
+    
+    # save user
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
 
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, ondelete='CASCADE')
-    content = db.Column(db.String(120))
-    approved = db.Column(db.Boolean)
-    created_at = db.Column(db.DateTime)
+    # delete user
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-class Department(db.Model):
-    __tablename__ = 'departments'
+class TokenBlocklist(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    jti = db.Column(db.String, nullable=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    description = db.Column(db.String(120))
+    def __repr__(self):
+        return  f"<Token {self.jti}>"
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
 
-class Event(db.Model):
-    __tablename__ = 'events'
+    # member_status = db.Column(db.Boolean, default=True)
+    # admin_status = db.Column(db.Boolean, default=False)
+    # super_admin_status = db.Column(db.Boolean, default=False)
 
-    id = db.Column(db.Integer, primary_key=True)
-    department_id = db.Column(db.Integer)
-    name = db.Column(db.String(120))
-    description = db.Column(db.String(120))
-    date = db.Column(db.DateTime)
-    location = db.Column(db.String(120))
-    organizer_info = db.Column(db.String(120))
+    # role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+    # personal_info_id = db.Column(db.Integer, db.ForeignKey('personal_info.id'))
 
-    department_id = db.Column(db.Integer, db.ForeignKey('department.id'))
+    # personal_info = db.relationship('PersonalInfo', backref='user')
+    # comments = db.relationship('Comment', backref='user')
+    # donations = db.relationship('Donation', backref='user')
+    # form_submissions = db.relationship('FormSubmission', backref='user')
+    # roles = db.relationship('Role', backref='user')
+    # events = db.relationship('Event', backref='user')
 
-class Sermon(db.Model):
-    __tablename__ = 'sermons'
+# class Role(db.Model):
+#     __tablename__ = 'roles'
 
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120))
-    date = db.Column(db.DateTime)
-    video_url = db.Column(db.String)
-    audio_url = db.Column(db.String)
-    notes = db.Column(db.String)
-    scripture = db.Column(db.String)
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(64))
 
-class Donation(db.Model):
-    __tablename__ = 'donations'
+# class PersonalInfo(db.Model):
+#     __tablename__ = 'personalinfos'
 
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer)
-    amount = db.Column(db.Float)
-    donation_date = db.Column(db.DateTime)
+#     id = db.Column(db.Integer, primary_key=True)
+#     user_id = db.Column(db.Integer)
+#     full_name = db.Column(db.String(120))
+#     address = db.Column(db.String(120))
+#     phone_number = db.Column(db.String(120))
+#     email = db.Column(db.String(120))
 
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+# class Comment(db.Model):
+#     __tablename__ = 'comments'
 
-class FormSubmission(db.Model):
-    __tablename__ = 'formsubmissions'
+#     id = db.Column(db.Integer, primary_key=True)
+#     user_id = db.Column(db.Integer, ondelete='CASCADE')
+#     content = db.Column(db.String(120))
+#     approved = db.Column(db.Boolean)
+#     created_at = db.Column(db.DateTime)
 
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer)
-    form_type = db.Column(db.String)
-    content = db.Column(db.String)
-    subitted_at = db.Column(db.DateTime)
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+# class Department(db.Model):
+#     __tablename__ = 'departments'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String)
+#     description = db.Column(db.String(120))
+
+# class Event(db.Model):
+#     __tablename__ = 'events'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     department_id = db.Column(db.Integer)
+#     name = db.Column(db.String(120))
+#     description = db.Column(db.String(120))
+#     date = db.Column(db.DateTime)
+#     location = db.Column(db.String(120))
+#     organizer_info = db.Column(db.String(120))
+
+#     department_id = db.Column(db.Integer, db.ForeignKey('department.id'))
+
+# class Sermon(db.Model):
+#     __tablename__ = 'sermons'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(120))
+#     date = db.Column(db.DateTime)
+#     video_url = db.Column(db.String)
+#     audio_url = db.Column(db.String)
+#     notes = db.Column(db.String)
+#     scripture = db.Column(db.String)
+
+# class Donation(db.Model):
+#     __tablename__ = 'donations'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     user_id = db.Column(db.Integer)
+#     amount = db.Column(db.Float)
+#     donation_date = db.Column(db.DateTime)
+
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+# class FormSubmission(db.Model):
+#     __tablename__ = 'formsubmissions'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     user_id = db.Column(db.Integer)
+#     form_type = db.Column(db.String)
+#     content = db.Column(db.String)
+#     subitted_at = db.Column(db.DateTime)
+
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
