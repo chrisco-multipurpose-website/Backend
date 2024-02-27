86fb524990e1ca4286483b4598c45b9a9ee186b5
Elikibs <elishakibet67@gmail.com>
2024-02-27 15:56:53 +0300

    update youtube blueprint

--- server/blueprints/youtube.py
+++ server/blueprints/youtube.py
@@ -3,7 +3,7 @@ import requests
 
 youtube_bp = Blueprint('youtube', __name__)
 
-YOUTUBE_API_KEY = 'AIzaSyDIlf92Bucc_PV3bShQdsrmtVAcTL5PbAk'
+YOUTUBE_API_KEY = 'AIzaSyAtaDVYFWfyLA-KMcOJLfWCzT_mWGpQYIc'
 CHANNEL_ID = 'UCx3_YD5Skpopibo0OUGKL0Q'
 YOUTUBE_API_URL = 'https://www.googleapis.com/youtube/v3/search'
 
